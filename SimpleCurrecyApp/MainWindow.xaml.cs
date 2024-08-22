fa7e55ab0196f26e09202019965605a183247692
Swallow-Desktop <lmh2157@outlook.kr>
2024-08-22 22:55:32 +0900

    Google Sheet를 통한 csv 환률 정보를 기반으로 환률 불러오기 성공

--- SimpleCurrecyApp/MainWindow.xaml.cs
+++ SimpleCurrecyApp/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System.Data;
+using System.Diagnostics;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
@@ -17,25 +18,57 @@ namespace SimpleCurrecyApp
     /// </summary>
     public partial class MainWindow : Window
     {
+    public static DataTable dtCurrency;
+    public static DataTable dtCurrencyList;
     public MainWindow()
         {
             InitializeComponent();
-            txtResult.Text = "test result";
+            dtCurrency = Data.CsvReader.LoadCsvToDataTable("Resources/ConcurrencyData.csv");
 
-        }
+            cmbFromUnit.ItemsSource = dtCurrency.DefaultView;
+            cmbFromUnit.DisplayMemberPath = "Name";
+            cmbFromUnit.SelectedValuePath = "Rate";
+            cmbFromUnit.SelectedIndex = 0;
 
-
-        private void BindCurrency()
-        {
-            DataTable dtCurrency = new DataTable();
+            cmbToUnit.ItemsSource = dtCurrency.DefaultView;
+            cmbToUnit.DisplayMemberPath = "Name";
+            cmbToUnit.SelectedValuePath = "Rate";
+            cmbToUnit.SelectedIndex = 0;
         }
 
         private void Convert_Click(object sender, RoutedEventArgs e)
         {
+            decimal fromval;
+            if (!decimal.TryParse(txtUserInput.Text, out fromval))
+            {
+                
+                WarningWindow.Show(Data.WarningType.NoUserInput, this);
+
+            } else if (cmbFromUnit.SelectedValue == null || cmbFromUnit.SelectedValue == "0")
+            {
+                WarningWindow.Show(Data.WarningType.NoFromCurrency, this);
+            } else if (cmbToUnit.SelectedValue == null || cmbToUnit.SelectedValue == "0")
+            {
+                WarningWindow.Show(Data.WarningType.NoToCurrency, this);
+            } else
+            {
+                decimal toval = Math.Round(fromval / decimal.Parse(cmbFromUnit.SelectedValue.ToString()) * decimal.Parse(cmbToUnit.SelectedValue.ToString()), 2);
+                txtResult.Text = toval.ToString();
+            }
         }
+
         private void Clear_Click(object sender, RoutedEventArgs e)
         {
-            txtResult.Text = string.Empty;
+            txtUserInput.Text = "";
+            txtResult.Text = "";
+            cmbFromUnit.SelectedIndex = 0;
+            cmbToUnit.SelectedIndex = 0;
+        }
+
+        private void Hyperlink_RequestNavigate(object sender, RequestNavigateEventArgs e)
+        {
+            Process.Start(new ProcessStartInfo(e.Uri.AbsoluteUri) { UseShellExecute = true });
+            e.Handled = true;
         }
     }
 }
---
+++ SimpleCurrecyApp/WarningWindow.xaml.cs
@@ -0,0 +1,82 @@
+﻿using Accessibility;
+using SimpleCurrecyApp.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace SimpleCurrecyApp
+{
+    /// <summary>
+    /// WarningWindow.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class WarningWindow : Window
+    {
+
+        public WarningType? Warn { get; set; }
+        public MainWindow? ParentWindow { get; set; }
+        
+        public WarningWindow()
+        {
+            InitializeComponent();
+        }
+
+        public static void Show(WarningType type, Window owner)
+        {
+            WarningWindow warningWindow = new WarningWindow()
+            {
+                Owner = owner
+        
+            };
+            warningWindow.Warn = type;
+            warningWindow.ParentWindow = owner as MainWindow;
+
+            if (Data.WarningType.NoUserInput == type)
+            {
+                warningWindow.txtWarning.Text = "Please enter a valid number";
+            }
+            else if (Data.WarningType.NoFromCurrency == type)
+            {
+                warningWindow.txtWarning.Text = "Please select a currency to convert from";
+            }
+            else
+            {
+                warningWindow.txtWarning.Text = "Please select a currency to convert to";
+            }
+            warningWindow.ShowDialog();
+        }
+
+
+        private void btnClose_Click(object sender, RoutedEventArgs e)
+        {
+            this.DialogResult = true;
+            if (ParentWindow == null)
+            {
+                return;
+            }
+
+            if (Warn == WarningType.NoUserInput)
+            {
+                ParentWindow?.txtUserInput.Focus();
+            }
+            else if (Warn == WarningType.NoFromCurrency)
+            {
+                ParentWindow?.cmbFromUnit.Focus();
+            }
+            else
+            {
+                ParentWindow?.cmbToUnit.Focus();
+            }
+        }
+    }
+    }
+
